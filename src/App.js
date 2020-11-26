96554f722e19a6a00d9da07232c14ad859f9ca44
jiluwei <1650302014@qq.com>
2020-11-26 18:40:47 +0800

    redux
    测试

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import {bindActionCreators, createStore, combineReducers} from 'redux';
+function run(){
+  const initialState = {count: 0};
+
+  //reducer
+  const counter = (state = initialState, action) => {
+    switch (action.type) {
+      case 'PLUS_ONE':
+        return { count: state.count + 1};
+      case 'MINUS_ONE':
+        return { count: state.count - 1};
+      case 'CUSTOM_COUNT':
+        return { count: state.count + action.payload.count};
+      default:
+        break;
+    }
+    return state;
+  }
+
+  function plusOne(){
+    return {type: 'PLUS_ONE'};
+  }
+
+  function minusOne(){
+    return {type: 'MINUS_ONE'};
+  }
+
+  function customCount(count){
+    return {type: 'CUSTOM_COUNT', payload: { count }}
+  }
+
+  const todos = (state ={}) => state;
+  const store = createStore(combineReducers({
+    counter,
+    todos,
+  }))
+
+
+  store.subscribe(() => console.log(store.getState()));
+
+  plusOne = bindActionCreators(plusOne, store.dispatch);
+  minusOne = bindActionCreators(minusOne, store.dispatch);
+  customCount = bindActionCreators(customCount, store.dispatch);
+  plusOne();
+  minusOne();
+  //测试
+  customCount(5);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={run}>Run</button>
     </div>
   );
 }
